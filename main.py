6705b09bbe5a9a222edc78f1659c8b14082a4444
Gerald Lee <gerald.lee@spatineo.com>
2022-08-26 10:16:31 +0300

    WIP: daily commit

--- main.py
+++ main.py
@@ -8,23 +8,28 @@ import uuid
 from shapely.geometry import Polygon
 from shapely.validation import explain_validity
 
+namespaces = {'lud-core': "http://tietomallit.ymparisto.fi/mkp-ydin/xml/1.2",
+              'splan': "http://tietomallit.ymparisto.fi/kaavatiedot/xml/1.2"}
+
 
 def main():
-    # validateXML()
-    # validateSchema()
-    # validatePlanIdentifier()
-    validateGeometrics()
-    return
-
-
-def validateXML():
     message = []
     try:
-        ET.parse('virallinenKaatio.xml')
+        tree = ET.parse('virallinenKaatio.xml')
         print('XML-tiedostolla on yksi root ja jokaisella avaavalla tagilla on lopetustagi')
     except ET.ParseError as err:
         message.append(err)
         return message
+    root = tree.getroot()
+
+    # validateSchema()
+    # validatePlanIdentifier(root)
+    # validateBoundary(root)
+    # validateGeom_b(root)
+    # findGmlIds(root)
+    # createObjectIdentifier(root, tree)
+
+    return
 
 
 def validateSchema():
@@ -54,46 +59,84 @@ def validateSchema():
         print('False')
 
 
-def validatePlanIdentifier():
-    tree = ET.parse('virallinenKaatio.xml')
-    root = tree.getroot()
+def validatePlanIdentifier(xml_root):
+
     try:
-        for child in root[0][0]:
-            get_planIdentifier = child.tag.split('}', 1)[0] + '}planIdentifier'
-            if child.tag == get_planIdentifier:
-                if len(child.text) > 0:
-                    print("PlanIdentifier tagissa on tekstiä")
-                    return True
+        first_featureMember: ET.Element = xml_root.find(
+            'lud-core:featureMember', namespaces)
+        first_spatialPlan = first_featureMember.find(
+            'splan:SpatialPlan', namespaces)
+        planIdentifier = first_spatialPlan.find(
+            'splan:planIdentifier', namespaces)
+        if len(planIdentifier.text) > 0:
+            print("PlanIdentifier tagissa on tekstiä")
+            return True
     except TypeError:
-        print("PlanIdentifier has no text")
+        print("No text in planIdentifier")
+
+
+def createObjectIdentifier(xml_root, xml_tree):
+    new_id = f"id-{uuid.uuid4()}"
+    first_featureMember = xml_root.find(
+        'lud-core:featureMember', namespaces)
+    first_spatialPlan = first_featureMember.find(
+        'splan:SpatialPlan', namespaces)
+    spatial_plan = ET.Element(first_spatialPlan)
+    objectIdentifier = ET.SubElement(
+        spatial_plan, '{http://tietomallit.ymparisto.fi/mkp-ydin/xml/1.2}objectIdentifier')
+    objectIdentifier.text = new_id
+    first_spatialPlan.insert(0, objectIdentifier)
+    xml_tree.write(open('output.xml', 'wb'), encoding="UTF-8")
+
+    # objectIdentifier = first_spatialPlan.find(
+    #     'lud-core:objectIdentifier', namespaces)
+    # if objectIdentifier:
+    #     print("True")
+    # else:
+    #     print("false")
+
+
+def findGmlIds(xml_root):
+    gml_id_dict = {}
+    for child in xml_root.iter():
+        for key, value in child.attrib.items():
+            if "id" in key:
+                gml_id_dict[value] = {"old_gml_id": value}
+    checkAllLinks(gml_id_dict, xml_root)
+
+
+def checkAllLinks(gmlDict, xml_root):
+    for child in xml_root.iter():
+        for key, value in child.attrib.items():
+            if "href" in key and "#" in value:
+                get_link_value = value.split('#', 1)[1]
+                if gmlDict.get(get_link_value) == None:
+                    print(f"{get_link_value} Does not exist as a gml_id")
+                else:
+                    print(f"{get_link_value} is a gml_id")
+
     return
 
 
-def validateGeometrics():
-    tree = ET.parse("virallinenKaatio.xml")
-    root = tree.getroot()
-    first_posList = []
-    second_posList = []
-    for index in range(0, 2):
-        for child in root[index].iter():
-            n = 2
-            get_posList = child.tag.split('}', 1)[0] + '}posList'
-            if child.tag == get_posList:
-                line = child.text
-                if index == 0:
-                    convert_to_array = child.text.split()
-                    first_posList = [(convert_to_array[i], convert_to_array[i + 1])
-                                     for i in range(0, len(convert_to_array), 2)]
-                else:
-                    convert_to_array = child.text.split()
-                    second_posList = [(convert_to_array[i], convert_to_array[i + 1])
-                                      for i in range(0, len(convert_to_array), 2)]
+def validateBoundary(xml_root):
+    first_featureMember = xml_root.find('lud-core:featureMember', namespaces)
+    first_spatialPlan = first_featureMember.find(
+        "splan:SpatialPlan", namespaces)
+    first_boundary = first_spatialPlan.find("lud-core:boundary", namespaces)
+    gml_geometry = first_boundary.find("*")
+    result = ET.tostring(gml_geometry, encoding='utf8', method='xml')
 
-    # TÄLLÄ HETKELLÄ NUMEROT OVAT STRING-MUODOSSA -> PITÄÄKÖ MUUTTAA DECIMAL-MUOTOON????
-    # lud-core:LandUseFeatureCollection oleva esim xmlns:splan -> jos splan:jotain, niin se tulee automaattisesti xml-elementin tagiin
-    print(first_posList)
-    print(second_posList)
+    pygml_result = pygml.parse(result)
 
+
+def validateGeom_b(xml_root):
+    second_featureMember_planObject = xml_root[1].find(
+        'splan:PlanObject', namespaces)
+    second_featureMember_geometry = second_featureMember_planObject.find(
+        'splan:geometry', namespaces)
+    gml_geometry_b = second_featureMember_geometry.find('*')
+    result = ET.tostring(gml_geometry_b, encoding='utf8', method='xml')
+    pygml_result = pygml.parse(result)
     return
 
 
@@ -102,3 +145,8 @@ if __name__ == '__main__':
 
 # Lataa kaikki schemat ja muuta linkit -> './...' muotoon
 # ID generointi Priority 2: 1. kohdan Geometriat ovat valideja kohdan jälkeen
+# found_jotain = root.find(
+#     './lud-core:featureMember/splan:SpatialPlan', namespaces)
+######################
+# for-loopissa child.set("updated", "yes") asettaa attribuutin elementille
+# jos pistää for child in root.iter("{...}featureMember") niin käy läpi kaikki featurememberit
