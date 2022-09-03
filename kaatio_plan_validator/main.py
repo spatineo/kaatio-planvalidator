f2452d4f205ee40b09c4ae576b15298cf2c503a2
Mikko Solismaa <mikko.solismaa@spatineo.com>
2022-09-03 16:37:03 +0000

    chore: add some api documentation

--- kaatio_plan_validator/main.py
+++ kaatio_plan_validator/main.py
@@ -2,7 +2,13 @@ from fastapi import FastAPI
 
 from .api_v1 import routes
 
-app = FastAPI()
+app = FastAPI(
+    contact={
+        "name": "Spatineo Inc.",
+        "email": "support@spatineo.com",
+    },
+    title="Kaava-XML:n simuloitu tallennuspalvelu",
+)
 
 
 app.include_router(
